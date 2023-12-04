03835d1d8301320deaa4fec4120f2b6b90135aaa
Dimas Septyanto <gn.dimas@gmail.com>
2023-12-04 11:45:24 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+  </div>
+</template>
+
+
+<script>
+  import Header from './components/Header.vue'
+  import Balance from './components/Balance.vue'
+  export default {
+    components: {
+      Header,
+      Balance
+    }
+  }
+</script>
