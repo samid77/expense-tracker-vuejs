2fa5df5110c24613ccf9cf5d4874a733889bfd1a
Dimas Septyanto <gn.dimas@gmail.com>
2023-12-05 13:11:27 +0700

    Add the remaining component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
   </div>
 </template>
 
@@ -12,12 +14,14 @@
   import Balance from './components/Balance.vue'
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import TransactionList from './components/TransactionList.vue';
+  import AddTransaction from './components/AddTransaction.vue';
   export default {
     components: {
       Header,
       Balance,
       IncomeExpenses,
       TransactionList,
+      AddTransaction
     }
   }
 </script>
